47964ed4ddebb01c26519581c121c37838db82b0
SamuelRamirezG <111009060+SamuelRamirezG@users.noreply.github.com>
2024-11-08 20:50:43 -0500

    Update settings.py

--- manejador_pagos/manejador_pagos/settings.py
+++ manejador_pagos/manejador_pagos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-($tfwsc#g=ml-%@x2)!_lxyn-#-&ox03spw0v3wd*_wpdmpydk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
